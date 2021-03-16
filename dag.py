5181a1218a502c6b5eb46f40482d43f35e7f114b
akaymaram <67651871+akaymaram@users.noreply.github.com>
2021-03-15 21:21:00 -0400

    added weight calculation and refactored code

--- dag.py
+++ dag.py
@@ -1,3 +1,5 @@
+# dag.py 1.0.0
+
 import networkx as nx
 from matplotlib import pyplot as plt
 from networkx.drawing.nx_agraph import graphviz_layout
@@ -5,77 +7,33 @@ from networkx.drawing.nx_agraph import graphviz_layout
 
 graph = nx.DiGraph()
 
-#Dict = {1: "sample sentece one.", 2: "sample sentence 2.", 3: "sample sentence three.", 4: "sample sentence four."}
+myfile = open("input.txt", "r")
+content = myfile.read()
+myfile.close()
 
-def maketuples():
-    myfile = open("input.txt", "r")
-    content = myfile.read()
-    myfile.close()
+list_of_sentences = [sentence for sentence in content.split(".") if len(sentence) > 0]
+print(list_of_sentences)
 
-    content_list = content.split(".")
-    # seems to create an empty string
-    content_list = content_list[:len(content_list) - 1]
-
-    dicts = {}
-    keys = range(len(content_list))
-    for i in keys:
-        dicts[i] = content_list[i]
-
-    #print(dicts)
-
-    # generate a list of tuples
-    list = [*range(0, len(content_list))]
-    res = []
-    for i in list:
-        temp = list[i + 1:]
-        for j in temp:
-            res.append((i, j))
-            j = j + 1
-        i = i + 1
-
-    return (res)
+for i in range(0, len(list_of_sentences)):
+	set_of_words_in_the_sentence = {word for word in list_of_sentences[i].split()}
+	for j in range(i+1, len(list_of_sentences)):
+		count = 0
+		for word_in_next_sentence in list_of_sentences[j].split():
+			if word_in_next_sentence in set_of_words_in_the_sentence:
+				count+=1
+		graph.add_edges_from([(i, j)], weight=count)
+		j = j + 1
+	i+= 1
 
 
-e = maketuples()
-graph.add_edges_from(e, weight=11)
-
-
-#graph.add_edges_from([(1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4)], weight=10)
 pos = nx.spring_layout(graph, seed=7)
 
 # plt.tight_layout()
 # nx.draw_networkx_nodes(graph, pos, node_size=700)
 # nx.draw_networkx_edges(graph, pos, width=6)
-nx.draw_networkx_edges(graph,pos,edge_color='b',alpha = 0.6)  # draws edges
+nx.draw_networkx_edges(graph,pos,edge_color='b',alpha = 0.6)
 nx.draw_networkx_edge_labels(graph,pos,edge_labels = nx.get_edge_attributes(graph,'weight'))
 nx.draw_networkx(graph, pos,arrows=False)
 plt.show()
 # plt.savefig("g.png", format="PNG")
 plt.clf()
-
-
-
-
-# from matplotlib import pyplot as plt
-# import networkx as nx
-# G = nx.Graph()
-# G.add_edge(1,2)
-# G.add_edge(2,3)
-# # for v in G.nodes():
-# #     G.node[v]['state']='X'
-# # G.node[1]['state']='Y'
-# # G.node[2]['state']='Y'
-
-# for n in G.edges_iter():
-#     G.edge[n[0]][n[1]]['state']='X'
-# G.edge[2][3]['state']='Y'
-
-# pos = nx.spring_layout(G)
-
-# nx.draw(G, pos)
-# node_labels = nx.get_node_attributes(G,'state')
-# nx.draw_networkx_labels(G, pos, labels = node_labels)
-# edge_labels = nx.get_edge_attributes(G,'state')
-# nx.draw_networkx_edge_labels(G, pos, labels = edge_labels)
-# plt.savefig('this.png')
-# plt.show()
