f4a094d8a2241d7acac2b7e5a6ce4e772080ba7a
Tina00000000 <80646096+Tina00000000@users.noreply.github.com>
2021-03-25 22:57:52 -0400

    Stop word removal, Normalization, lemmatization

--- dag.py
+++ dag.py
@@ -3,28 +3,57 @@
 import networkx as nx
 from matplotlib import pyplot as plt
 from networkx.drawing.nx_agraph import graphviz_layout
-
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 graph = nx.DiGraph()
 
-myfile = open("input.txt", "r")
+myfile = open("input2.txt", "r")
 content = myfile.read()
 myfile.close()
 
-list_of_sentences = [sentence for sentence in content.split(".") if len(sentence) > 0]
-print(list_of_sentences)
+
+## removing stop words and Lemmatization
+stop_words = set(stopwords.words('english'));
+word_tokens = word_tokenize(content)
+lemmatizer = WordNetLemmatizer()
+filtered_sentence = []
+for w in word_tokens:
+    if w.lower() not in stop_words:
+        filtered_sentence.append(lemmatizer.lemmatize(w))
+final_sentence= ' '.join(filtered_sentence)
+
+
+
+list_of_sentences = [sentence for sentence in final_sentence.split(".") if len(sentence) > 0]
+
+list_count =[]
 
 for i in range(0, len(list_of_sentences)):
 	set_of_words_in_the_sentence = {word for word in list_of_sentences[i].split()}
+
 	for j in range(i+1, len(list_of_sentences)):
 		count = 0
 		for word_in_next_sentence in list_of_sentences[j].split():
-			if word_in_next_sentence in set_of_words_in_the_sentence:
+			if word_in_next_sentence in set_of_words_in_the_sentence and word_in_next_sentence != ",":
+				print(word_in_next_sentence)
 				count+=1
-		graph.add_edges_from([(i, j)], weight=count)
+		list_count.append(count)
 		j = j + 1
 	i+= 1
 
+
+##Normalization of edge weights
+max_count = max(list_count)
+k=0
+for i in range(0, len(list_of_sentences)):
+	for j in range(i + 1, len(list_of_sentences)):
+		count=list_count[k]
+		graph.add_edges_from([(i, j)], weight=count/max_count)
+		k=k+1
+		j = j + 1
+	i += 1
 
 pos = nx.spring_layout(graph, seed=7)
 
